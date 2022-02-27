109cc060d3c2da949953d35478b693b4795f5fa5
Unmesh Gundecha <upgundecha@gmail.com>
2022-02-27 17:26:51 +0800

    Change spec

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -6,12 +6,12 @@ describe('Sign up', () => {
 
   it('should register a new user', () => {
 
-    const name = cy.faker.name.firstName() + ' ' + cy.faker.name.lastName();
+    const userName = cy.faker.name.findName();
     const email = cy.faker.internet.email();
 
     cy.get('a[href*=register]').click();
     cy.get('button').should('have.text', 'Sign Up');
-    cy.get('input[name=username]').type(name);
+    cy.get('input[name=username]').type(userName);
     cy.get('input[name=email]').type(email);
     cy.get('input[name=password]').type('P@ssword');
     cy.get('button.btn-primary').click();
